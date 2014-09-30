ef53b29d26eabe53c122923ee1b8a3fecd0ee95c
Nicolás Hock Isaza <nhocki@gmail.com>
2014-09-29 22:29:16 -0700

    Adding `content_or_partial` method
    This method will render the section of `content` if `content_for` was called
    or will look for a partial to render instead.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def content_or_partial(content, partial = nil)
+    partial ||= content
+    content_for?(content) ? content_for(content) : render( content.to_s)
+  end
 end
