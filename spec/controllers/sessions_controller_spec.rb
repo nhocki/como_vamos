e2de82c0642a8566ee31b8dfda6658c923a0a593
Nicolás Hock Isaza <nhocki@gmail.com>
2014-10-19 10:32:44 -0500

    Adding really simple controller spec for Sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,4 +1,13 @@
 require "rails_helper"
 
 RSpec.describe SessionsController, :type => :controller do
+  before do
+    mock_github_omniauth
+    controller.env["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
+  end
+
+  it "redirects the user back to the root url" do
+    get :create, provider: 'github'
+    expect(subject).to redirect_to(root_url)
+  end
 end
