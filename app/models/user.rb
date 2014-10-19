f1d5cec08c29c64a96b790652511a0d8cb6114bf
Nicolás Hock Isaza <nhocki@gmail.com>
2014-10-19 09:38:24 -0500

    Adding method to find or create users from OmniAuth hashes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
   validates :name, :provider, presence: true
   validates :username, :email, presence: true, uniqueness: true
   validates :provider_uid, presence: true, uniqueness: { scope: :provider }
+
+  def self.from_omniauth(auth)
+    info = auth['info']
+    User.where(provider: auth['provider'], provider_uid: auth['uid']).first_or_create! do |user|
+      user.name = info['name']
+      user.email = info['email']
+      user.username = info['nickname']
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,30 @@ RSpec.describe User, :type => :model do
     it { expect validate_uniqueness_of([ :username, :email, :provider_uid ]) }
     it { expect validate_presence_of([ :username, :name, :provider, :email, :provider_uid ]) }
   end
+
+  describe ".from_omniauth" do
+    let(:omniauth_hash) do
+      {
+        provider: "github",
+        uid: "123",
+        info: {
+          nickname: "nhocki",
+          email: "nhocki@gmail.com",
+          name: "Nicolás Hock Isaza",
+          image: "https://avatars.githubusercontent.com/u/17035?v=2",
+        }.stringify_keys
+      }.stringify_keys
+    end
+
+    it "creates a user if it doesn't exist already" do
+      expect {
+        User.from_omniauth(omniauth_hash)
+      }.to change(User, :count).from(0).to(1)
+    end
+
+    it "finds the user with the same provider and uid" do
+      user = create(:user, provider: 'github', provider_uid: '123')
+      expect(User.from_omniauth(omniauth_hash)).to eql(user)
+    end
+  end
 end
