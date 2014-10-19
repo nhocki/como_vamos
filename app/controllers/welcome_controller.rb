a6c5451268c5cd18b7b2314b87b5ce30f2648fc9
Nicolás Hock Isaza <nhocki@gmail.com>
2014-10-19 10:11:06 -0500

    Don't let users go to Regwall if they are not logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+  before_filter :require_not_logged_in!, only: [ :regwall ]
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
-  get '/regwall' => 'welcome#regwall'
+  get '/regwall' => 'welcome#regwall', as: :regwall
   root 'welcome#index'
 end
