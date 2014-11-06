23a4bb15b838380bfae1aeebdd622f486639376c
Nicolás Hock Isaza <nhocki@gmail.com>
2014-11-05 21:04:29 -0800

    Move factory sequences to their own file.
    Keep model factories small and just related to that model.

--- spec/factories/judges.rb
+++ spec/factories/judges.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :judge do
-    name "Uva Online Judge"
-    url "http://uva.onlinejudge.org/"
-    slug "uva-online-judge"
+    name { generate(:name) }
+    url  { generate(:url) }
     problems_count 1
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,4 @@
 FactoryGirl.define do
-  sequence :uid do |n|
-    "uid_#{n}"
-  end
-
-  sequence :email do |n|
-    "email#{n}@example.com"
-  end
-
   factory :user do
     provider 'github'
     provider_uid { generate(:uid) }
