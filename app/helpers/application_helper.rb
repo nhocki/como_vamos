6f395e6813f962a58898eab6e3b66ad1c772af84
Nicolás Hock Isaza <nhocki@gmail.com>
2014-11-19 00:27:31 -0800

    Removing dummy views & helpers
    There were great when starting the design and we had no models, but not needed
    anymore.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,27 +12,7 @@ module ApplicationHelper
     "/auth/#{provider}"
   end
 
-  def random_problem
-    Problem.all.to_a.sample
-  end
-
-  def random_solution
-    Solution.all.to_a.sample
-  end
-
-  def random_category(limit = 1)
-    sidebar_categories.sample(limit)
-  end
-
   def sidebar_categories
     @sidebar_categories ||= Category.order('name')
-  end
-
-  def random_user
-    User.all.sample
-  end
-
-  def random_judge
-    Judge.all.sample
   end
 end
