6273da768a5e3a82a7724d05c8c7a4427f50bdd3
Nicolás Hock Isaza <nhocki@gmail.com>
2014-11-24 22:16:19 -0800

    Make avatar have a default space even if the image fails to load.

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -2,7 +2,7 @@ class ProblemsController < ApplicationController
   before_action :require_login!, except: [ :index, :show ]
 
   def index
-    @problems = Problem.order(:created_at)
+    @problems = Problem.order('updated_at DESC')
   end
 
   def show
