9e0f11513e9149720f8c4b0b28fb66c8b628875f
Nicolás Hock Isaza <nhocki@gmail.com>
2014-12-06 19:35:13 -0800

    Adding specs for SolutionPolicy#download?
    Make sure only logged in users can download solutions.

--- spec/policies/solution_policy_spec.rb
+++ spec/policies/solution_policy_spec.rb
@@ -1,9 +1,10 @@
 require "spec_helper"
 
 describe SolutionPolicy do
+  let(:solution) { create(:solution, user: user) }
+  let(:user) { create(:user) }
+
   describe "#update?" do
-    let(:user) { create(:user) }
-    let(:solution) { create(:solution, user: user) }
     let(:policy) { SolutionPolicy.new(user, solution) }
 
     it "is true when the user owns the policy" do
@@ -13,6 +14,18 @@ describe SolutionPolicy do
     it "is false when the solution is for another user" do
       policy = SolutionPolicy.new(create(:user), solution)
       expect(policy.update?).to be_falsy
+    end
+  end
+
+  describe "#download?" do
+    it "is true when the user is logged in" do
+      policy = SolutionPolicy.new(user, solution)
+      expect(policy.download?).to be_truthy
+    end
+
+    it "is false when the user not is logged in" do
+      policy = SolutionPolicy.new(User.new, solution)
+      expect(policy.download?).to be_falsy
     end
   end
 end
