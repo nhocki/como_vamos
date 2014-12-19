ac0c2ed695b1738db0884c920a9223ffd33277d2
Nicolás Hock Isaza <nhocki@gmail.com>
2014-12-19 09:57:13 -0500

    Localize "no problem" text

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -1,7 +1,13 @@
 module ProblemsHelper
-	def without_problems
-		unless controller.controller_name =~ /users/
-			"There are no problems here yet, you can #{link_to "add one here", new_problem_path}.".html_safe
-		end
-	end
+  def without_problems
+    unless controller.controller_name =~ /users/
+      default_no_problems_text
+    end
+  end
+
+  def default_no_problems_text
+    add_link = link_to t('.add_link'), new_problem_path
+    t('.no_problems', add_link: add_link).html_safe
+  end
 end
+
