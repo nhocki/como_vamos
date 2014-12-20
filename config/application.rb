7b1a0e3aa601eb84f5cf9c0a9c3be8269a415ade
Nicolás Hock Isaza <nhocki@gmail.com>
2014-12-20 18:21:28 -0500

    Update Rails files
    `rake rails:update`

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module ComoVamos
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Bogota'
 
+    # Do not ignore errors in after_commit or after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
